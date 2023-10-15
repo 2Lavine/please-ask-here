da791e2c823d78814cfcef1b8ec96d78961df506
bigideahuang <bigideahuang@foxmail.com>
2023-10-15 08:09:20 +0800

    remove baseURL

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,6 +1,6 @@
 const baseUrl = 'http://localhost:3000';
 const api = {
-  REGISTER: `${baseUrl}/api/auth/register`,
-  SIGNIN: `${baseUrl}/api/auth/pwd-login`,
+  REGISTER: `/api/auth/register`,
+  SIGNIN: `/api/auth/pwd-login`,
 };
 export const { REGISTER, SIGNIN } = api;
